d36881a4e130e671acb0d5a659f1b6dfdbabb5b2
Boosbol <learntofly844@gmail.com>
2026-06-25 09:27:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "./",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    modulePreload: {
-      polyfill: false,
-    },
     target: "es2015",
-    rollupOptions: {
-      output: {
-        format: "iife", 
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]"
-      }
-    }
+    outDir: "dist",
   }
-}));
+});
